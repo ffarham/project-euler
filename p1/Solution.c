67c747520aefd57dee4c42595fae855f02d4bfe5
Farham Ahmad <u2012839@warwick.ac.uk>
2021-04-29 22:46:42 +0100

    adding solution in c

---
+++ p1/Solution.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    int i = 0, total = 0;
+    for(i = 0; i < 1000; i++){
+        if(i%3 == 0 || i%5 == 0){total += i;}
+    }
+    printf("total: %d\n", total);
+}
