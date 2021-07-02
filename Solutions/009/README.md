d608ff392786a16c76e0270105b5d95b8e01e65f
Farham Ahmad <u2012839@warwick.ac.uk>
2021-07-03 00:30:36 +0100

    added README and updated python file

---
+++ Solutions/009/README.md
@@ -0,0 +1,5 @@
+There exists exactly one Pythagorean triplet for which a + b + c = 1000. Find the product abc.
+
+The solution uses brute force to try all combinations to find values for a,b and c.
+
+Ans: 31875000
--- Solutions/009/Solution.py
+++ Solutions/009/Solution.py
@@ -1,9 +1,9 @@
-for i in range(300):
-   for k in range(500):
-    for j in range(500):
-        if i*i + k*k == j*j:
-            if i+j+k == 1000:
-                P = i*j*k
-                print(i,j,k,P)
-
-print("end")
+if __name__ == "__main__":
+    for i in range(300):
+        for k in range(500):
+            for j in range(500):
+                if i*i + k*k == j*j:
+                    if i+j+k == 1000:
+                        P = i*j*k
+                        print("Ans: " + str(P))
+                        exit()
